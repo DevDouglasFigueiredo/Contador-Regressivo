4724193f91bcd03a38c9c812d41c922ee12b7354
Douglas Figueiredo <devdouglasfigueiredo@gmail.com>
2022-03-08 17:30:14 -0300

    Contador Regressivo

---
+++ contador.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const formatarDigito = (digito) => `0${digito}`.slice(-2);
+
+    const atualizar = (tempo) => {
+        const segundos = document.getElementById( 'segundos' );
+        const minutos = document.getElementById( 'minutos' );
+
+        const qtdSegundos = tempo % 60;
+        const qtdMinutos = Math.floor((tempo % (60 * 60)) / 60);
+
+        segundos.textContent = formatarDigito(qtdSegundos);
+        minutos.textContent = formatarDigito(qtdMinutos);
+    }
+
+
+const contagemResgressiva = (tempo) => {
+    const pararContagem = () => clearInterval(id);
+
+
+const contar = () =>{
+
+        if (tempo === 0){
+            pararContagem();}
+
+        atualizar(tempo);
+        tempo --;
+    }
+    const id = setInterval(contar,1000);
+    
+    }
+
+contagemResgressiva(92);
+
+
+
